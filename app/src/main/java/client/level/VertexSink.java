1afdabc80f9f7572f96fa858bf6f42acec97ffd2
7mWalee <141520325+7mWalee@users.noreply.github.com>
2026-05-26 00:31:47 +0300

    Delete app directory

--- app/src/main/java/client/level/VertexSink.java
+++
@@ -1,8 +0,0 @@
-package client.level;
-
-public interface VertexSink {
-    void normal(float x, float y, float z);
-    void color(float r, float g, float b);
-    void texture(float u, float v);
-    void vertex(float x, float y, float z);
-}
